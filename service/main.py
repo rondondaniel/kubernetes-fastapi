f1853eebee960767ab4a0454ee1c627c42044565
Daniel Rondon <rondondaniel@gmail..com>
2022-01-06 18:10:13 +0100

    Last changes

--- service/main.py
+++ service/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from service.api.api_v1.api import router as api_router
 from service.core.config import API_V1_STR, PROJECT_NAME
@@ -7,6 +8,15 @@ app = FastAPI(
     title=PROJECT_NAME,
     # if not custom domain
     # openapi_prefix="/prod"
+)
+
+# NEW
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix=API_V1_STR)
